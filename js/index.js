0912172bca4f9eab89f7b948edff2ba4d8c71d93
WangYuliang <langzi1206@msn.cn>
2020-05-21 17:49:19 +0800

    some change

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+// Every Vue application starts by creating a new Vue instance with the Vue function.
+// We often use the variable vm (short for ViewModel) to refer to our Vue instance.
+var vm = new Vue({
+  el: '#vm',
+  // When a Vue instance is created, it adds all the properties found in its data object to Vue's reactivity system.
+  // It should be noted that if you use Object.freeze(), it will prevent existing properties from being changed, which also means the reactivity system cannot track changes.
+  data: {
+    text: 'hi',
+    num: 0,
+    condition: false,
+    array: [],
+    error: null
+  },
+  // Each Vue instance runs functions called lifecycle hooks, giving users the opportunity to add their own code at specific stages.
+  created: function () {
+    // 'this' point to the vm instance
+    console.log(this + ' created')
+  },
+  mounted: function () {
+    console.log(this + ' mounted')
+  },
+  updated: function () {
+    console.log(this + ' updated')
+  },
+  destroyed: function () {
+    console.log(this + ' destroyed')
+  }
+})
+
+// In addition to data properties, Vue instances expose a number of useful instance properties and methods.
+// These are prefixed with '$' to differentiate them from user-defined properties. For example: 
+console.log("vm.$el === document.getElementById('vm') is " + (vm.$el === document.getElementById('vm')))
+
 var declarative = new Vue({
   el: "#declarative",
   data: {
